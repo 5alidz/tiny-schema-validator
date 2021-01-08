7036bdacc10dcf4cad0dbf8cc9fce7b3f33ffed2
khaled <kzack-dev@hotmail.com>
2021-01-08 16:26:23 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,5 @@
-import { isPlainObject, UnknownKey } from './utils';
-import { Validator, ObjectValidator } from './validators';
-import { createValidate, ValidationError } from './validate';
+import { UnknownKey } from './utils';
 
+export * from './createSchema';
+export * from './helpers';
 export const dynamicObjectKey = UnknownKey;
-
-export interface Schema {
-  [key: string]: Validator;
-}
-
-interface ISchema<T> {
-  validate: (data: T) => ValidationError[] | null;
-  createObjectValidator: (config?: { optional: boolean }) => ObjectValidator;
-  produce: (data: T) => T;
-}
-
-export function createSchema<T>(_schema: Schema): Readonly<ISchema<T>> {
-  if (!isPlainObject(_schema)) {
-    throw new Error('schema should be a valid object');
-  }
-
-  const schema = Object.freeze({ ..._schema });
-  const validate = createValidate<T, Schema>(schema);
-
-  return Object.freeze({
-    validate,
-    createObjectValidator(config = { optional: false }): ObjectValidator {
-      return Object.freeze({ type: 'object', shape: schema, ...config });
-    },
-    produce(data: T): T {
-      const errors = validate(data);
-      if (errors) throw errors;
-      return data;
-    },
-  });
-}
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 export const UnknownKey = Symbol.for('schema.object.unknown');
 
+export const safeSpread = (o?: Record<string, unknown>) => (o ? o : {});
+
 export function isPlainObject(maybeObject: unknown) {
   return (
     typeof maybeObject == 'object' &&
